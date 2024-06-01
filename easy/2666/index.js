1c2fefab3a48f1fc5eaf4841a2235da0b936611e
IvanYaremko <ivan.yaremko@gmail.com>
2024-06-01 16:02:22 +0100

    commit

---
+++ easy/2666/index.js
@@ -0,0 +1,11 @@
+const once = function(fn) {
+  let flag = true
+  return function(...args){
+      if(flag){
+          flag = false
+          return fn(...args)
+      }
+      return undefined
+  }
+  
+};
