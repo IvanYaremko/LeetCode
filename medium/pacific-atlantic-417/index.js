3b1bd2f6f22f82bacd798391e02ed6b06e228000
IvanYaremko <ivan.yaremko@gmail.com>
2024-06-29 11:40:48 +0100

    commit

--- medium/pacific-atlantic-417/index.js
+++ medium/pacific-atlantic-417/index.js
@@ -11,14 +11,28 @@ var pacificAtlantic = function(matrix) {
  }
  
  for (let col=0; col<COLS;col++){
-     dfs(matrix, 0, col, Number.MIN_SAFE_INTEGER, pacific)
-     dfs(matrix, ROWS - 1, col, Number.MIN_SAFE_INTEGER, atlantic)
+     dfs(0, col, Number.MIN_SAFE_INTEGER, pacific)
+     dfs(ROWS - 1, col, Number.MIN_SAFE_INTEGER, atlantic)
  }
   
-  for (let row = 0; row<matrix.length; row++){
-      dfs(matrix, row, 0, Number.MIN_SAFE_INTEGER, pacific)
-      dfs(matrix, row, COLS - 1, Number.MIN_SAFE_INTEGER, atlantic)
+  for (let row = 0; row<ROWS; row++){
+      dfs(row, 0, Number.MIN_SAFE_INTEGER, pacific)
+      dfs(row, COLS - 1, Number.MIN_SAFE_INTEGER, atlantic)
   }
+
+  function dfs(i, j, prev, ocean){
+    //checkOutOfBounds
+      if (i<0 || i > matrix.length -1 || j < 0 || j > matrix[i].length - 1) return
+      if (matrix[i][j] < prev) return
+      if (ocean[i][j]) return
+      ocean[i][j] = true
+      
+      dfs(i+1, j, matrix[i][j], ocean)
+      dfs(i-1, j, matrix[i][j], ocean)
+      dfs(i, j+1, matrix[i][j], ocean)
+      dfs(i, j-1, matrix[i][j], ocean)    
+    }
+    
   
   let res = []
   for (let i=0;i<ROWS;i++){
@@ -31,22 +45,4 @@ var pacificAtlantic = function(matrix) {
   return res
 }
 
-const dfs = (matrix, i, j, prev, ocean) =>{
-//checkOutOfBounds
-  if (i<0 ||
-     i > matrix.length -1 ||
-     j < 0 ||
-     j > matrix[i].length - 1
-    ) return
-  
-  
-  if (matrix[i][j] < prev) return
-  if (ocean[i][j]) return
-  ocean[i][j] = true
-  
-  dfs(matrix, i+1, j, matrix[i][j], ocean)
-  dfs(matrix, i-1, j, matrix[i][j], ocean)
-  dfs(matrix, i, j+1, matrix[i][j], ocean)
-  dfs(matrix, i, j-1, matrix[i][j], ocean)    
-}
 
