0143c20a6d6cb89f1553af5d021b3053ede1f779
IvanYaremko <ivan.yaremko@gmail.com>
2024-07-02 19:56:23 +0100

    commit

---
+++ medium/130/index.js
@@ -0,0 +1,32 @@
+var solve = function(board) {
+  for(let i = 0; i < board.length; i++){
+   for(let j = 0; j < board[0].length; j++){
+       if(board[i][j] === 'O' && (
+           i === 0 || i === board.length-1 || j === 0 || j === board[0].length-1 
+       )){
+           dfs(i, j)
+       }
+   }
+  }
+
+  function dfs(i, j){
+   if(i < 0 || i >= board.length || j < 0 || j >= board[i].length || board[i][j] === 'X' || board[i][j] === 'M') return
+   board[i][j] = 'M'
+   dfs(i+1, j)
+   dfs(i-1, j)
+   dfs(i, j+1)
+   dfs(i, j-1)
+   return
+  }
+
+  for(let i = 0; i < board.length; i++){
+   for(let j = 0; j < board[0].length; j++){
+       if(board[i][j] === 'M'){
+           board[i][j] = 'O'
+       }else{
+           board[i][j] = 'X'
+       }
+   }
+  }
+  return board
+};
